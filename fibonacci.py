6db65637cb0f248aa45fac9c1a12c182acfa3784
Liam Allan <lallan03@uoguelph.ca>
2025-10-03 02:42:41 -0400

    Improve input validation and error handling
    Fixed input validation and exception handling in fibonacci.py.

--- fibonacci.py
+++ fibonacci.py
@@ -4,21 +4,22 @@ def fibonacci_sequence():
     try:
       n = int(input("Enter number of terms of the fibonacci sequence"))
 
-if n<= 0:
+  if n<= 0:
   print("Enter a positive integer")
   continue
 
-a, b =  0, 1
-print("\nfibonacci_sequence:")
-for _ in range(n):
+  a, b =  0, 1
+  print("\nfibonacci_sequence:")
+  for _ in range(n):
   print(a, end=" ")
   a, b = b, a + b
   Print()
   break
 
-except InputValueMistake:
+except ValueError:
 print("Invalid integer, please enter a positive integer")
 
+fibonacci_sequence()
 # Fibonacci Sequence Exercise
 # TODO: (Read detailed instructions in the Readme file)
 # Prompt the user for the number of terms.
